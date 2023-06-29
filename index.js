6136dd883b43ddbd66e604dcf2a35170c63b64e9
Oscar Saavedra <oscarsaavedra06@gmail.com>
2023-06-29 17:40:40 -0500

    ajustes en archivos yml y puerto del api

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.delete('/users/:id', async (req, res) => {
 });
 
 // Start the server
-app.listen(3001, () => {
+app.listen(3000, () => {
   // console.log('Server is running on port 3001');
 });
 
